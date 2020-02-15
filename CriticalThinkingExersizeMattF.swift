1fc80ed7a22b605ac16202e477c2721413789b35
Ammy28 <47332786+Ammy28@users.noreply.github.com>
2020-02-14 21:14:58 -0600

    Rename app.swift to CriticalThinkingExersizeMattF.swift

---
+++ CriticalThinkingExersizeMattF.swift
@@ -0,0 +1,5 @@
+var celsius = 0.0
+var fahrenheit = 0.0
+
+fahrenheit = (celsius × 9/5) + 32 
+print(fahrenheit)
--- app.swift
+++
@@ -1,5 +0,0 @@
-var celsius = 0.0
-var fahrenheit = 0.0
-
-fahrenheit = (celsius × 9/5) + 32 
-print(fahrenheit)
