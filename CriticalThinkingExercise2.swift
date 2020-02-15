1aa3a2a53de4b363e416dff372312c865f7b00ed
Ammy28 <47332786+Ammy28@users.noreply.github.com>
2020-02-14 21:36:46 -0600

    Add files via upload

---
+++ CriticalThinkingExercise2.swift
@@ -0,0 +1,19 @@
+var a = 10
+var b = 20
+var c = 6
+var d = 14
+
+
+
+print(a != b)
+print((b-c) == d)
+print(d>c)
+print(a<b)
+print((c*b)>=d)
+print((b/a)<=c)
+print(d+c==b)
+print(a%c)
+var e = (a>b ? "a is greater": "b is greater")
+print(e)
+print((a != b)&&(c>b))
+print((a != b)||(c>b))
