0d9a57dc7ed6023828506bea3b5d2d1c075e8eed
Ammy28 <47332786+Ammy28@users.noreply.github.com>
2020-03-08 19:18:13 -0500

    Add files via upload

---
+++ fibonacci.swift
@@ -0,0 +1,13 @@
+var num1 = 0
+var num2 = 1
+var num3 = 1
+ 
+ while(num2<1000) {
+    
+        let num = num1 + num2
+        num1 = num2
+        num2 = num
+  print("result =", num2)
+
+    }
+ 
